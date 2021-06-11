d486da01918093bbf76a61f619caf737d1c22ba0
Mikhail Rantovich <mrantovich@gmail.com>
2021-06-11 23:39:01 +0700

    Исправления мелких недостатков работы счётчика слайдера на главной

--- js/main.js
+++ js/main.js
@@ -16,24 +16,14 @@ for (i = 0; i < sliderDotsArray.length; i++) {
 	sliderDotsArray[i].addEventListener('click', doSliderDotClick);
 };
 const sliderBGDictionary = {
-	'slider-dot-01': ['0', '01'],
-	'slider-dot-02': ['-100vw', '02'],
-	'slider-dot-03': ['-200vw', '03'],
-	'slider-dot-04': ['-300vw', '04'],
+	'slider-dot-01': ['0', '01', 0],
+	'slider-dot-02': ['-100vw', '02', 1],
+	'slider-dot-03': ['-200vw', '03', 2],
+	'slider-dot-04': ['-300vw', '04', 3],
 };
 
 const mainBGContainer = document.querySelector('.main__bg-container');
 const sliderVNumber = document.querySelector('.main__slider-vertical-number');
-
-function doSliderDotClick() {
-	for (i = 0; i < sliderDotsArray.length; i++) {
-		sliderDotsArray[i].classList.remove('main__slider-vertical-dots-item_current');
-	};
-	this.classList.add('main__slider-vertical-dots-item_current');
-	var keyToSlider = this.id;
-	mainBGContainer.style.left = sliderBGDictionary[keyToSlider][0];
-	sliderVNumber.innerHTML = sliderBGDictionary[keyToSlider][1];
-};
 
 let curSlider = 1;
 const maxSlider = 3;
@@ -43,6 +33,19 @@ function autoSlider() {
 	if (curSlider > maxSlider) {
 		curSlider = 0;
 	};
+};
+
+function doSliderDotClick() {
+	for (i = 0; i < sliderDotsArray.length; i++) {
+		sliderDotsArray[i].classList.remove('main__slider-vertical-dots-item_current');
+	};
+	this.classList.add('main__slider-vertical-dots-item_current');
+	var keyToSlider = this.id;
+	mainBGContainer.style.left = sliderBGDictionary[keyToSlider][0];
+	sliderVNumber.innerHTML = sliderBGDictionary[keyToSlider][1];
+
+	curSlider = sliderBGDictionary[keyToSlider][2];
+	console.log(curSlider);
 };
 
 window.setInterval(autoSlider, 5000);
